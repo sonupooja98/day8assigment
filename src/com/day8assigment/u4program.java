c2570281076f0cd856ed39d0ed1968f3a8aeaf23
sonupooja98 <91020442+sonupooja98@users.noreply.github.com>
2021-10-06 22:07:27 +0530

    Update u4program.java

--- src/com/day8assigment/u4program.java
+++ src/com/day8assigment/u4program.java
@@ -5,7 +5,7 @@ public class u4program {
     public static final int IS_PART_TIME = 2;
     public static final int IS_FULL_TIME = 1;
     public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 2;
+    public static final int NUM_OF_WORKING_DAYS = 20;
     public static int empWage() {
         int empHrs = 0;
         int empWage = 0;
